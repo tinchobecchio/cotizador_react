f64fbc761a1155e71cd432ee0e5847f25e5c2e29
Martin Becchio <tincho.becchio@gmail.com>
2023-03-29 18:05:22 -0300

    agregando styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
+import styled from "@emotion/styled"
+
+const ContenedorHeader = styled.header`
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #ffffff;
+`
+
+const TextoHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`
+
 
 const Header = ({titulo}) => {
     return ( 
-        <header>
-            <h1>{titulo}</h1>
-        </header>
+        <ContenedorHeader>
+            <TextoHeader>{titulo}</TextoHeader>
+        </ContenedorHeader>
     );
 }
  
