cb1a5d08fdc27af81234e84de7d54196b329d576
Martin Becchio <tincho.becchio@gmail.com>
2023-03-29 18:15:08 -0300

    styled components en app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import Header from "./components/Header";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`
+
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`
+
 
 function App() {
   return (
-    <Header 
-      titulo={'Cotizador de Seguros'}
-    />
+    <Contenedor>
+      <Header 
+        titulo={'Cotizador de Seguros'}
+      />
+
+      <ContenedorFormulario>
+        
+      </ContenedorFormulario>
+    </Contenedor>
+    
   );
 }
 
