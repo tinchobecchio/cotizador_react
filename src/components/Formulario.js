4673f4f2840786bba1c7846f3537953a604b45da
Martin Becchio <tincho.becchio@gmail.com>
2023-03-30 00:01:18 -0300

    styled component al button

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -19,7 +19,23 @@ const Select = styled.select`
 const InputRadio = styled.input`
     margin: 0 1rem;
 `
+const Boton = styled.button`
+    background-color: #00838f;
+    font-size: 16px;
+    width: 100%;
+    padding: 1rem;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    border: none;
+    transition: background-color 0.3s ease;
+    margin-top: 2rem;
 
+    &:hover {
+        background-color: #26c6da;
+        cursor: pointer;
+    }
+`
 
 
 const Formulario = () => {
@@ -67,7 +83,7 @@ const Formulario = () => {
                 /> Completo
             </Campo>
 
-            <button type="button">Cotizar</button>
+            <Boton type="button">Cotizar</Boton>
         </form>
     );
 }
